6e0ba4168f7c6b0f19ec06ad062861eee08b891a
alexmatiasas <alexmatiasas@users.noreply.github.com>
2024-10-29 11:39:45 +0000

    deploy: 479c68fb87ff34ec6191de69b9f54fdd9812f918

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Post de ejemplo",
+        "excerpt":"Este es el contenido de mi primer post en el blog. Aquí puedo escribir sobre mis experiencias, proyectos o cualquier cosa relevante a mi portafolio.  ","categories": ["Blog"],
+        "tags": ["Machine Learning","Data Science"],
+        "url": "/blog/example/",
+        "teaser": "/assets/images/teaser.jpg"
+      },{
+    "title": "About Me",
+    "excerpt":"I am a Data Scientist and Machine Learning Engineer with a passion for solving complex problems with data.   Skills     Programming: Python, R, SQL   Machine Learning: Scikit-learn, TensorFlow, PyTorch   Data Visualization: Matplotlib, Seaborn, Plotly   Experience     Include any relevant experience or accomplishments here.   akl  ","url": "https://alexmatiasas.github.io/about/"
+  },{
+    "title": "Blog",
+    "excerpt":"Welcome to my blog, where I share insights and tutorials on Data Science, Machine Learning, and more.  ","url": "https://alexmatiasas.github.io/blog/"
+  },{
+    "title": "Contact",
+    "excerpt":"Feel free to reach out through LinkedIn or send me an email at alejandromatiasastorga@gmail.com.  ","url": "https://alexmatiasas.github.io/contact/"
+  },{
+    "title": "Posts",
+    "excerpt":"","url": "https://alexmatiasas.github.io/posts/"
+  },{
+    "title": "Welcome to My Data Science Portfolio",
+    "excerpt":"Hi, I’m Manuel Alejandro!  I’m a Data Scientist and Machine Learning Engineer with expertise in [mention key skills].   ","url": "https://alexmatiasas.github.io/"
+  },{
+    "title": "Projects",
+    "excerpt":"Below is a showcase of my Data Science and Machine Learning projects.  ## My Projects  # Preparation of the work environment  In this section, we will prepare our working environment by installing and loading the necessary libraries. Proper setup is essential for efficient data handling, visualization, and analysis in R. This step ensures that all the tools and libraries we need are readily available for our exploration and modeling tasks.  ## Installing libraries  In this step, we install the necessary libraries for our analysis, including packages for data manipulation, visualization, text mining, and word clouds. Each package serves a unique purpose in the data science workflow, as detailed below:  -   **tidyverse**: For data manipulation and visualization. -   **data.table**: Optimized for handling large datasets efficiently. -   **ggplot2**: Essential for creating a wide variety of visualizations. -   **dplyr**: Used for data wrangling and filtering. -   **tm**: Text mining for natural language processing (NLP). -   **wordcloud**: Generates word clouds for text data visualization. -   **readr**: Efficient data reading. -   **knitr**: Supports notebook creation and reporting.  ``` # Install necessary libraries (only first time) # install.packages(c(\"tidyverse\", \"data.table\", \"ggplot2\", \"dplyr\", \"tm\", \"wordcloud\", \"knitr\", \"tm\")) ```","url": "https://alexmatiasas.github.io/projects/"
+  }]
